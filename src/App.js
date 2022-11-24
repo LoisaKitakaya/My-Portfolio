5e9a670aebc7f0d782b73303ee95214beb8ea459
Freedom Loisa <kitakayaloisa@gmail.com>
2022-11-24 19:54:33 +0300

    new landing page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 
+import LandingPage from "./pages/LandingPage";
+
 function App() {
   return (
     <div className="App">
       {/* routes */}
-      <Routes></Routes>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
       {/* routes */}
     </div>
   );
