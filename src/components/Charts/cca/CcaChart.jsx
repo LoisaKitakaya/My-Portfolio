59b1f0f7f983e72b32227cf41e64a548946c3eb8
Freedom Loisa <kitakayaloisa@gmail.com>
2022-11-29 14:09:03 +0300

    setting up charts

--- src/components/Charts/cca/CcaChart.jsx
+++ src/components/Charts/cca/CcaChart.jsx
@@ -1,10 +1,33 @@
 import { Component } from "react";
+import {
+  BarChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  Bar,
+} from "recharts";
 
 class CcaChart extends Component {
   render() {
     const { chartData } = this.props;
+    console.log(chartData);
 
-    return <p className="text-xl text-zinc-500">Chart comes here</p>;
+    return (
+      <>
+        <BarChart width={700} height={350} data={chartData} className="mb-8">
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="timestamp" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="additions" fill="#02a6e3" />
+          <Bar dataKey="deletions" fill="#dc3c30" />
+          <Bar dataKey="commits" fill="#478e37" />
+        </BarChart>
+      </>
+    );
   }
 }
 
